568682515009892c1261ac21d3efd08a1bb55037
Andersonlimaz <andersonlimaz@outlook.com>
2023-05-23 10:37:39 -0300

    Robo Pronto

--- ExtrairTitulo/Robot1.py
+++ ExtrairTitulo/Robot1.py
@@ -1,20 +1,27 @@
 from imap_tools import MailBox, AND
 import openpyxl
-login = "estagiario1.naj.prfn3@pgfn.gov.br"
-senha = "hzrctlidbdooqvax"
+import re 
+
+login = "push.demandas.prfn3regiao@pgfn.gov.br"
+senha = "smybwsrarkugrcls"
 
 
 meu_email = MailBox("imap.gmail.com").login(login, senha)
 
-# Pegar email que foram enviados por um remetente especifico 
 workbook = openpyxl.Workbook()
 sheet = workbook.active
 
-lista_emails = meu_email.fetch(AND(from_="andersonzlima7@gmail.com"))
+lista_emails = meu_email.fetch(AND(from_="pje@trf3.jus.br"))
 
 for email in lista_emails:
     subject = email.subject
+    split_subject = subject.split()
     
-    sheet.append([subject])
+    if len(split_subject) >= 5:
+        quinto_split = split_subject[4]
+    else:
+        quinto_split = "Não disponível"
+    
+    sheet.append([quinto_split])
 
 workbook.save("emails.xlsx")
